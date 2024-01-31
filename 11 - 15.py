994b5c33bf1e0556234c3a1ca0229660d0322724
ItsSlaver <157491943+ItsSlaver@users.noreply.github.com>
2024-01-31 15:13:45 +0300

    Add files via upload

---
+++ 11 - 15.py
@@ -0,0 +1,31 @@
+print("11")
+v = int(input())
+m = int(input())
+P = m / v
+print("Ответ: ",P)
+print("12")
+a = int(input())
+b = int(input())
+c = a + b
+d = a - b
+e = a * b
+f = a // b
+print("Ответ: ", c, d, e, f)
+print("13")
+a = int(input())
+b = int(input())
+am = (a + b) / 2
+gm = (a * b) ** 2
+print("Ответ: ", am, gm)
+print("14")
+S = int(input())
+Kol = int(input())
+Plot = S // Kol
+print("Ответ: ", Plot)
+print("15")
+a = int(input())
+b = int(input())
+c = int(input())
+S = 2 * (a * b + a * c + b * c)
+V = a * b * c
+print("Ответ: ", S, V)
